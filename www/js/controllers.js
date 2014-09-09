6e5b78c53d027cf8ccbdd40b08889d7f0c37cde8
Martin <madetho@live.de>
2014-09-10 00:08:50 +0200

    Userlist and messages are now shown

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,52 +1,61 @@
 angular.module('starter.controllers', ['services'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, socket) {
-  // Form data for the login modal
-  $scope.loginData = {};
-  $scope.messages = [
-    { usr: 'Martin', msg: 'Hello' },
-    { usr: 'cc', msg: 'Hi' },
-    { usr: 'Martin', msg: 'Nigga' },
-    { usr: 'Martin', msg: 'Bla' },
-    { usr: 'cc', msg: 'WWWWWWWWWWWWWWWWW :)' }
-  ];
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, socket) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.users = [];
+    $scope.messages = [];
+    $scope.lastMessages = [];
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-    $scope.modal.show();
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+        $scope.modal.show();
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-      
-    var usernameVal = $scope.loginData.username;
-	var passwordVal = $scope.loginData.password;
-	socket.emit('sign in', {
-		username: usernameVal,
-		password: passwordVal
-	}, function (user) {
-		if (user) {
-            $scope.closeLogin();
-            socket.emit('get user status');
-	        socket.emit('get old messages');
-            socket.emit('get emoticons');
-		} else {
-			alert('Login failed.');
-		}
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
+
+        var usernameVal = $scope.loginData.username;
+        var passwordVal = $scope.loginData.password;
+        socket.emit('sign in', {
+            username: usernameVal,
+            password: passwordVal
+        }, function (user) {
+            if (user) {
+                $scope.closeLogin();
+                socket.emit('get user status');
+                socket.emit('get old messages');
+                socket.emit('get emoticons');
+            } else {
+                alert('Login failed.');
+            }
+        });
+    };
+
+    socket.on('user status update', function (userlist) {
+        $scope.users = userlist;
+    });
+    
+    socket.on('load old messages', function (messages) {
+        $scope.lastMessages  = messages;
 	});
-  };
+
+    socket.on('new message', function(message){
+        $scope.messages.push(message);
+    });
+
 })
