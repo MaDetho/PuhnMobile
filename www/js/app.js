61f109e366c40de694f1948ea4814a6eff754aa6
Martin <madetho@live.de>
2014-09-09 22:36:53 +0200

    Make it Ionic SlideBox

--- www/js/app.js
+++ www/js/app.js
@@ -22,23 +22,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
     .state('app', {
       url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
+      templateUrl: "templates/chat.html",
       controller: 'AppCtrl'
-    })
-
-    .state('app.chat', {
-      url: "/chat",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/chat.html"
-        }
-      }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/chat');
+  $urlRouterProvider.otherwise('/app');
 });
 
